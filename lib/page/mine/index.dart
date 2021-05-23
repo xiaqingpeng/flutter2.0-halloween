bd474a123820e41b9187a15203b613ac3b4adb5d
xiaqingpeng <626143872@qq.com>
2021-05-23 21:41:50 +0800

    路由导航

---
+++ lib/page/mine/index.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import './widget/shared_preferences.dart';
+class MinePage extends StatefulWidget {
+  final routes = {
+    '/shared_preferences': (BuildContext context) => new SharedPreferences(),
+  };
+
+  @override
+  _MinePageState createState() => _MinePageState();
+}
+
+class _MinePageState extends State<MinePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false, // 去除debugger
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('我的'),
+            centerTitle: true,
+          ),
+          body: ListView(
+            children: <Widget>[TabList()],
+          )),
+      routes: widget.routes,
+    );
+  }
+}
+
+class TabList extends StatelessWidget {
+  final List navigatorList = [
+    {'name': '状态存储', 'id': 4, 'icon': 0xe67f},
+  ];
+
+  Widget _listViewItemUi(BuildContext context, item) {
+    return Column(children: <Widget>[
+      Divider(
+        height: 2.0,
+        indent: 0.0,
+        color: Colors.grey,
+      ),
+      ListTile(
+        title: new Text(item["name"],
+            style: new TextStyle(
+              fontSize: 12.0,
+              color: Colors.black54,
+            )),
+        onTap: () {
+          Navigator.push(
+            context,
+            new MaterialPageRoute(
+              // ignore: missing_return
+              builder: (BuildContext context) {
+                switch (item['name']) {
+                  case "状态存储":
+                    return SharedPreferences();
+
+                  default:
+                    return SharedPreferences();
+                }
+              },
+            ),
+          );
+          // Navigator.of(context).pushNamed('/mine_detail',arguments: item);
+        },
+        leading: new Icon(
+          // item["icon"],
+          IconData(item['icon'], fontFamily: 'iconfont'),
+          color: Colors.black54,
+        ),
+        trailing: Icon(Icons.keyboard_arrow_right),
+      ),
+    ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: navigatorList.map((item) {
+      return _listViewItemUi(context, item);
+    }).toList());
+  }
+}
